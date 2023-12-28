41fd5ce9cd5270a98d0e2f6265c2a2e3e763702a
PYUDNG <PY_DNG@163.com>
2023-12-29 00:57:01 +0800

    fix custom assets

--- custom-assets/custom-assets.js
+++ custom-assets/custom-assets.js
@@ -5,16 +5,16 @@ const cwd = process.cwd();
 mergeFolder(cwd, path.dirname(cwd), true);
 
 function mergeFolder(from, to, overwrite=false) {
-	console.log(`Merging directory ${from} to ${to}`);
+	debug(`Merging directory ${from} to ${to}`);
 	try { fs.accessSync(to, fs.constants.F_OK) } catch(e) { fs.mkdirSync(to); }
 
 	const items = fs.readdirSync(from, { withFileTypes: true });
 	for (const item of items) {
 		const item_path = path.resolve(item.path, item.name);
 		const dest_path = path.resolve(to, item.name);
-		console.log([item, item_path, dest_path]);
+		debug([item, item_path, dest_path]);
 		if (item.isFile()) {
-			console.log(`Copying file ${item_path} to ${dest_path}`);
+			debug(`Copying file ${item_path} to ${dest_path}`);
 			try {
 				fs.accessSync(dest_path, fs.constants.F_OK);
 				overwrite && fs.unlinkSync(dest_path);
@@ -24,7 +24,16 @@ function mergeFolder(from, to, overwrite=false) {
 		} else if (item.isDirectory()) {
 			mergeFolder(item_path, dest_path, overwrite);
 		} else {
-			console.log('Not a file nor a directory:' + item_path);
+			debug('Not a file nor a directory:' + item_path);
 		}
 	}
+}
+
+function debug(...args) {
+	!debug.count && (debug.count = 0);
+	debug.count++;
+
+	console.log(`[[ Debug Output ${debug.count} ]]`);
+	console.log(...args);
+	console.log();
 }
