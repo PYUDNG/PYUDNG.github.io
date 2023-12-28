ce04951eedb02d5e2ab4afc459ed7a44f1f3c668
PYUDNG <PY_DNG@163.com>
2023-12-29 00:41:09 +0800

    fix custom assets

--- custom-assets/custom-assets.js
+++ custom-assets/custom-assets.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const cwd = process.cwd();
-mergeFolder(cwd, path.dirname(cwd));
+mergeFolder(cwd, path.dirname(cwd), true);
 
 function mergeFolder(from, to, overwrite=false) {
 	console.log(`Merging directory ${from} to ${to}`);
