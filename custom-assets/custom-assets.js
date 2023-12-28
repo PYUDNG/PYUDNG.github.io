b3e9aed7475c4907b86da2739d7e2bef364d30aa
PYUDNG <PY_DNG@163.com>
2023-12-29 00:39:23 +0800

    fix custom assets

--- custom-assets/custom-assets.js
+++ custom-assets/custom-assets.js
@@ -15,7 +15,10 @@ function mergeFolder(from, to, overwrite=false) {
 		console.log([item, item_path, dest_path]);
 		if (item.isFile()) {
 			console.log(`Copying file ${item_path} to ${dest_path}`);
-			overwrite && fs.accessSync(dest_path, fs.constants.F_OK) && fs.unlinkSync(dest_path);
+			try {
+				fs.accessSync(dest_path, fs.constants.F_OK);
+				overwrite && fs.unlinkSync(dest_path);
+			} catch(e) {}
 			fs.copyFileSync(item_path, dest_path,
 				overwrite ? fs.constants.COPYFILE_FICLONE : fs.constants.COPYFILE_EXCL);
 		} else if (item.isDirectory()) {
