3286eb0b4ffbff10c8d9645086be267b3cf5d1b1
PYUDNG <PY_DNG@163.com>
2024-01-07 17:46:22 +0800

    new post; added custom-post.js for custom css and custom js in post pages; installed hexo-math, use Katex to render Latex

---
+++ scripts/custom-post.js
@@ -0,0 +1,2 @@
+hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="/css/custom-post-styles.css">', 'post');
+hexo.extend.injector.register('head_end', '<script src="/js/custom-post-js.js"></script>', 'post');
