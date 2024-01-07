cd518b386073cd028397b66ddab5e33777a2ad87
PYUDNG <PY_DNG@163.com>
2024-01-07 18:00:47 +0800

    debug

--- scripts/custom-post.js
+++ scripts/custom-post.js
@@ -1,2 +1,3 @@
 hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="/css/custom-post-styles.css">', 'post');
 hexo.extend.injector.register('head_end', '<script src="/js/custom-post-js.js"></script>', 'post');
+console.log('[custom-post] registered custom-post-styles and custom-post-js.');
